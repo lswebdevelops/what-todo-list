57e324ca04a1f86358288bcff3e7e68b383b8f8e
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-11-20 20:28:24 -0300

    Adding projects and tasks to the screen

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
+let myProjects = [];
+let newProject;
+
+class Project {
+  constructor(title) {
+      this.title = title; 
+  }
+}
+function addProjToMyProjects(title){
+  
+  newProject = new Project(title);
+  myProjects.push(newProject);
+  
+
+}
+
+function addProj(){
+  const inputTitleOfProj = document.getElementById("titleOfProj").value;
+ 
+  addProjToMyProjects(inputTitleOfProj);
+}
+
+ //printing the projects to the screen
+
+ const contentProj = document.getElementById('contentProj');
+
+ function createElementsForProjPrinting(){
+  deleteContentofProj()
+      function addParagraph(title){
+      const titleLabel = document.createElement('p')
+      titleLabel.textContent = `${title}`
+      contentProj.appendChild(titleLabel);
+           }
+                               
+           for (let i in myProjects){
+            let test = myProjects[i]
+            addParagraph(myProjects[i].title );
+         
+
+              console.log(myProjects);
+          }
+          console.log(" ")
+
+        
+}
+let buttonAddProjEvent = document.querySelector('.btnAddProj').addEventListener('click', buttonAddProj);
+
+function buttonAddProj(){
+  createElementsForProjPrinting();
+
+}
+
+function deleteContentofProj(){
+  // delete all below id='contentProj' when a button add Add Project is clicked
+  function empty(element) {
+    while(element.firstElementChild) {
+       element.firstElementChild.remove();
+    }
+    
+  }
+  
+  let parentProf = document.getElementById("contentProj");
+  empty(parentProf);
+ 
+}
+
+
+
+// adding tasks: 
+
+
 let myTasks = [];
 let newTask;
 // console.log(newTask);
@@ -6,7 +77,6 @@ class Task {
       this.title = title; 
       this.description = description; 
       this.dueDate = dueDate; 
-      
   }
 }
 function addTaskToMyTasks(title, description, dueDate){
@@ -16,32 +86,24 @@ function addTaskToMyTasks(title, description, dueDate){
   
   // console.log(newTask);
 }
-
-// adding a new book to collection
+// adding a new book to collection manually for testing(erase after project ready)
 // addTaskToMyTasks("supermarket", 'buy xampu','2022-11-11')
 // addTaskToMyTasks("supermarket2", 'buy xampu2','2022-11-12')
-
 // adding a new book to collection from inputs
 function addTask(){
   const inputTitle = document.getElementById("title").value;
   const inputDescription = document.getElementById("description").value;
   const inputDueDate = document.getElementById("dueDate").value;
-
-
- addTaskToMyTasks(inputTitle, inputDescription, inputDueDate);
+addTaskToMyTasks(inputTitle, inputDescription, inputDueDate);
  //printing the tasks to the console:
 // for (let i in myTasks){
 //   console.log(myTasks[i]);
   
 // }
 // console.log(" ")
-
 }
-
  //printing the tasks to the screen
-
  const content = document.getElementById('content');
-
  function createElementsForTaskPrinting(){
   deleteContent()
       function addParagraph(title, description, dueDate){
@@ -54,20 +116,15 @@ function addTask(){
             let test = myTasks[i]
             addParagraph(myTasks[i].title, myTasks[i].description,myTasks[i].dueDate );
          
-
-
+              console.log(myTasks);
           }
           console.log(" ")
-
         
 }
 let buttonAddTaskEvent = document.querySelector('.btnAddTask').addEventListener('click', buttonAddTask);
-
 function buttonAddTask(){
   createElementsForTaskPrinting();
-
 }
-
 function deleteContent(){
   // delete all below id='content' when a button add Task clicked
   function empty(element) {
@@ -81,9 +138,3 @@ function deleteContent(){
   empty(parent);
  
 }
-  
-
-
-
-
-
